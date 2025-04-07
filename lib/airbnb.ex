df632c887cb735adec46a0ed9204343c31596d46
OriHdzCodes <annaisa203@gmail.com>
2025-04-07 11:44:06 -0600

    Initial commit

---
+++ lib/airbnb.ex
@@ -0,0 +1,18 @@
+defmodule Airbnb do
+  @moduledoc """
+  Documentation for `Airbnb`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Airbnb.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
