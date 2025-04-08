f99b385ea0d879a7def4c3ae0437e1addc4e81c7
OriHdzCodes <annaisa203@gmail.com>
2025-04-07 20:13:49 -0600

    Filters neighbourhood and property

--- lib/airbnb.ex
+++ lib/airbnb.ex
@@ -1,5 +1,16 @@
 defmodule Airbnb do
-  def aggr_count_properties(sample_500.csv) do
-    :world
+  def aggr_count_properties(file_path) do
+    File.stream!(file_path)
+    |> CSV.decode(headers: true, escape_max_lines: 20, mode: :normal)
+    |> Enum.map(fn
+      {:ok, row} ->{row["neighbourhood_cleansed"], row["property_type"]}
+      {:error, _reason} ->
+        nil
+    end)
+    |> Enum.filter(& &1)
+    |> Enum.frequencies()
+    |> Enum.map(fn {{neighbourhood_cleansed, property_type}, count} ->
+      {neighbourhood_cleansed, property_type, count}
+    end)
   end
 end
